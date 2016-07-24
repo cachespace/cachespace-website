22024aaef1c6e7634dda2b5d964aeb55c39d893b
Jasmine Roberts <jasmineayodele@gmail.com>
2016-07-24 10:10:47 -0700

    adding website files

---
+++ js/tweet_api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'fk8m20jKlDJGAL6zihVdaazQh');
+    define('CONSUMER_SECRET', 'SoTc7Zlg8Ydb2yt1lpT5KqrpRbTNgaDqXvXM6GAd15ilPZJH9w');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '470562222-uHTVATIGZceY4d0PbrXEkTApAmZa1eyS3wusC8H3');
+    define('ACCESS_SECRET', '5uabZEQxcul3a2DjiSEh60ipFG7ZwbSfyzEh72PKGGnJA');
+    
+    // Cache Settings
+    define('CACHE_ENABLED', false);
+    define('CACHE_LIFETIME', 3600); // in seconds
+    define('HASH_SALT', md5(dirname(__FILE__)));
